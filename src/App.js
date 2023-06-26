0b1c78fe8552d8ee4d128076ff53b6b79f2990fe
EC2 Default User <ec2-user@ip-172-31-7-124.us-west-1.compute.internal>
2023-06-26 13:24:24 +0000

    added text input and submit button, added API responses to UI, added state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from "./logo.svg";
+import React, { useState } from 'react';
 import "@aws-amplify/ui-react/styles.css";
 import {
   withAuthenticator,
@@ -7,31 +8,52 @@ import {
   Image,
   View,
   Card,
-} from "@aws-amplify/ui-react";
+}
+from "@aws-amplify/ui-react";
 
 import { API } from 'aws-amplify';
 
-async function startGame() {
+function App({ signOut }) {
+  const [gameOutput, setGameOutput] = useState("");
+  const [userInput, setUserInput] = useState("");
+
+
+  async function startGame() {
     const apiName = 'apibcb6604c';
     const path = '/game/start';
     const options = {
-        headers: {}
+      headers: {}
     };
 
     const response = await API.post(apiName, path, options);
     console.log(response);
-}
+    setGameOutput(response.data.message); // update the game's output with response
+  }
 
+  async function sendInput() {
+    const apiName = 'myAPIName'; // replace with your API name
+    const path = '/game/{gameId}/input';
+    const options = {
+      headers: {}, // any additional headers
+      body: { input: userInput } // pass the user's input here
+    };
 
-function App({ signOut }) {
+    const response = await API.post(apiName, path, options);
+    console.log(response);
+    setGameOutput(response.data.message); // update the game's output with the new response
+  }
+
   return (
     <View className="App">
       <Card>
         <Image src={logo} className="App-logo" alt="logo" />
         <Heading level={1}>We now have Auth!</Heading>
+        <p>{gameOutput}</p>
+        <input value={userInput} onChange={e => setUserInput(e.target.value)} />
+        <Button onClick={sendInput}>Send Input</Button>
         <Button onClick={startGame}>Start Game</Button>
+        <Button onClick={signOut}>Sign Out</Button>
       </Card>
-      <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
 }
